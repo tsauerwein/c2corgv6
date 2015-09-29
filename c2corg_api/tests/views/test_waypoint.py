9e5d1be1425046b3a834d2f370e32481f187f6ec
tsauerwein <tobias.sauerwein@camptocamp.com>
2015-09-29 12:02:25 +0200

    Remove nature/is_minor/written_at columns

--- c2corg_api/tests/views/test_waypoint.py
+++ c2corg_api/tests/views/test_waypoint.py
@@ -99,10 +99,8 @@ class TestWaypointRest(BaseTestCase):
 
         self.assertEqual(version.culture, 'en')
         self.assertEqual(version.version, 1)
-        self.assertEqual(version.nature, 'ft')
 
         meta_data = version.history_metadata
-        self.assertEqual(meta_data.is_minor, False)
         self.assertEqual(meta_data.comment, 'creation')
         self.assertIsNotNone(meta_data.written_at)
 
